3658204512253452b3168033d47ce3228e1f446a
completejavascript <completejavascript.super@gmail.com>
2019-07-08 20:18:55 +0700

    Update layout category, tag page

--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import Layout from "../layout";
+import Header from "../components/Header/Header";
+import MainContainer from "../components/MainContainer/MainContainer";
+import Sidebar from "../components/Sidebar/Sidebar";
 import PostListing from "../components/PostListing/PostListing";
 import { getPostList } from "../utils/helpers";
-import Header from "../components/Header/Header";
 import config from "../../data/SiteConfig";
 
 export default class CategoryTemplate extends React.Component {
@@ -12,20 +14,15 @@ export default class CategoryTemplate extends React.Component {
     const { category, categoryList, tagList } = this.props.pageContext;
     const postEdges = this.props.data.allMarkdownRemark.edges;
     const postList = getPostList(postEdges);
+    const content = <PostListing postList={postList} hasThumbnail={true} />;
+    const sidebar = <Sidebar location="on_list" tagList={tagList} categoryList={categoryList} />;
 
     return (
       <Layout>
         <div className="category-container">
-          <Helmet
-            title={`Posts in category "${category}" | ${config.siteTitle}`}
-          />
+          <Helmet title={`Posts in category "${category}" | ${config.siteTitle}`} />
           <Header title={`Posts in category "${category}"`} />
-          <PostListing 
-            postList={postList} 
-            categoryList={categoryList}
-            tagList={tagList}
-            hasThumbnail={true}
-          />
+          <MainContainer content={content} sidebar={sidebar} />
         </div>
       </Layout>
     );
