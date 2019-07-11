f5235d969947a0f92bacde23b40e82b8b886a612
completejavascript <completejavascript.super@gmail.com>
2019-07-11 08:06:40 +0700

    Update logo, favicon, brand

--- src/components/Navigation/NavBrand.jsx
+++ src/components/Navigation/NavBrand.jsx
@@ -4,7 +4,7 @@ import AutoLink from "../AutoLink/AutoLink";
 const NavBrand = ({ brand, title }) => (
   <div className="brand">
       <AutoLink className="flex align-items-center" to="/">
-        <img className="image margin-right-half" src={brand} alt="Brand Image" />
+        <img className="image margin-right-half border-radius" src={brand} alt="Brand Image" />
         <span className="title"><strong>{title}</strong></span>
       </AutoLink>
     </div>
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer/Footer";
 import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 import siteConfig from "../../data/SiteConfig";
 import brand from "../../content/images/brand.png";
-import favicon from "../../content/images/favicon.png";
+import favicon from "../../content/images/favicon.ico";
 import "../components/Icons/FontAwesome";
 import "../styles/shiba.min.css";
 
@@ -13,7 +13,7 @@ const MainLayout = ({ children }) => (
   <div>
     <Helmet htmlAttributes={{ lang: siteConfig.siteLang }}>
       <meta name="description" content={siteConfig.siteDescription} />
-      <link rel="shortcut icon" type="image/png" href={favicon} />
+      <link rel="shortcut icon" type="image/x-icon" href={favicon} />
     </Helmet>
     <Navigation 
       brand={brand}
