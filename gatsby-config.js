8f1b8aa90a7e4f4f52f6c849f3fef77fad9ec30e
Lam Pham <completejavascript.super@gmail.com>
2021-11-09 22:42:07 +0700

    fix: remove gatsby-plugin-offine

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,7 +117,7 @@ module.exports = {
         ],
       },
     },
-    "gatsby-plugin-offline",
+    "gatsby-plugin-remove-serviceworker",
     {
       resolve: "gatsby-plugin-feed",
       options: {
